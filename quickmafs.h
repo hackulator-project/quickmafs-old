1e0c6abdc285e5fa2def7d28a96c70919e525881
Sreehari <sreeharisreedev1+cyka@gmail.com>
2017-12-23 12:31:25 -0800

    2+2 is 4 minus 1 that's 3 quick mafs. Everyday man's on the block. smoke trees

--- quickmafs.h
+++ quickmafs.h
@@ -1,12 +1,72 @@
-#ifndef __QUICKMAFS_H__
-#define __QUICKMAFS_H__
+#include<math.h>
 char *outtext;
-char peek();
-char get();
+char peek() {
+  return *outtext;
+}
+
+char get() {
+  return *outtext++;
+}
+
 double expression();
 
-double number();
-double factor();
-double term();
-double evaluate(const char *expr);
-#endif /*__QUICKMAFS_H__*/
+double number() {
+  double result = get() - '0';
+  while (peek() >= '0' && peek() <= '9') {
+    result = 10*result + get() - '0';
+  }
+  return result;
+}
+
+double factor() { //P
+  if (peek() >= '0' && peek() <= '9') {
+    return number();
+  }
+  else if (peek() == '(') {
+    get(); // '('
+    double result = expression();
+    get(); // ')'
+    return result;
+  }
+  else if (peek() == '-') {
+    get();
+    return -factor();
+  }
+  return 0; // error
+}
+double exponent() { //E
+  double result = factor();
+  while(peek() == '^' || peek() == 'r') {
+    if(get() == '^')
+      result = pow(result, factor());
+    if(get() == 'r')
+      result = pow(factor(), 1/result); // 3r27 becomes 27 to the power of 1/3
+
+  }
+}
+double term() //MD
+{
+  double result = exponent();
+  while (peek() == '*' || peek() == '/' || peek() == 'a')
+    if (get() == '*')
+      result *= exponent();
+    else
+      result /= exponent();
+  return result;
+}
+
+double expression() //AS
+{
+  double result = term();
+  while (peek() == '+' || peek() == '-')
+    if (get() == '+')
+      result += term();
+    else
+      result -= term();
+  return result;
+}
+double evaluate(const char *expr) {
+  outtext = (char*)expr;
+  double result = expression();
+  return result;
+}
